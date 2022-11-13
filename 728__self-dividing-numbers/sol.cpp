7f4e16b05b5f01cf0822e0667e81707cc00cccf7
Alexander Zsikla <alesandor00@gmail.com>
2022-11-13 14:59:04 -0500

    Reformatted file to use 2 spaces opposed to 4

--- 728__self-dividing-numbers/sol.cpp
+++ 728__self-dividing-numbers/sol.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    vector<int> selfDividingNumbers(int left, int right) {
-        vector<int> ans;
-        for (; left <= right; left++) {
-            if (left < 9) {
-              ans.push_back(left);
-              continue;
-            }
+  vector<int> selfDividingNumbers(int left, int right) {
+    vector<int> ans;
+    for (; left <= right; left++) {
+      if (left < 9) {
+        ans.push_back(left);
+        continue;
+      }
 
-            int num = left;
-            int digit;
-            bool is_dividing = true;
-            while (num) {
-              digit = num % 10;
-              if (digit == 0 || left % digit != 0) {
-                is_dividing = false;
-                break;
-              }
-              num /= 10;
-            }
-
-            if (is_dividing) ans.push_back(left);
+      int num = left;
+      int digit;
+      bool is_dividing = true;
+      while (num) {
+        digit = num % 10;
+        if (digit == 0 || left % digit != 0) {
+          is_dividing = false;
+          break;
         }
+        num /= 10;
+      }
 
-        return ans;
+      if (is_dividing) ans.push_back(left);
     }
+
+    return ans;
+  }
 };
