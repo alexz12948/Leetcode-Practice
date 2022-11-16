0f923ea56a86da05fe62770bd7548406cf10d18b
Alexander Zsikla <alesandor00@gmail.com>
2022-11-16 08:45:00 -0500

    updated documentation for q 26

--- 26__remove-duplicates-from-sorted-array/sol.cpp
+++ 26__remove-duplicates-from-sorted-array/sol.cpp
@@ -1,5 +1,19 @@
-/* Time: O(n)
- * Space: O(1) */
+/* 
+Explanation of Solution
+-----------------------
+
+Since the array is sorted, then we can use that information when trying to
+determine where to place the next unique. We have a pointer that points at the
+first unique element. The loop goes through all the elements and if the iterator
+does not match the pointer to the unique element, then it must mean that we are
+looking at a new element. It then we move the pointer up on spot and place the
+new unique element in there
+
+Complexity
+----------
+Time: O(n)
+Space: O(1)
+*/
 class Solution {
 public:
   int removeDuplicates(vector<int>& nums) {
