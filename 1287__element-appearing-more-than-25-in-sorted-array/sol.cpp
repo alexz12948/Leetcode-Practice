8ef0c94953e82181262587848502a182984af5d7
Alexander Zsikla <alesandor00@gmail.com>
2022-11-15 21:39:48 -0500

    added explanation to 1287

--- 1287__element-appearing-more-than-25-in-sorted-array/sol.cpp
+++ 1287__element-appearing-more-than-25-in-sorted-array/sol.cpp
@@ -1,5 +1,19 @@
-/* Time: O(n)
- * Space: O(1) */
+/*
+Explanation of Solution
+-----------------------
+
+Since the array is in sorted order, we can use that to our advantage. Inside of the
+loop, as long as the value is the same, we increament the count of the number of
+times a certain value was seen. If we move onto another value, we reset the count
+
+If the count every goes above 25% (or count * 4 > n), then 
+we know that must be the number
+
+Complexity
+----------
+Time: O(n)
+Space: O(1)
+*/
 class Solution {
 public:
   int findSpecialInteger(vector<int>& arr) {
