7925b80d523ec5ad56551d501468376f487885bc
Alexander Zsikla <alesandor00@gmail.com>
2022-11-16 23:07:37 -0500

    finished q 52

---
+++ 525__contiguous-array/sol.cpp
@@ -0,0 +1,46 @@
+/*
+Explanation of Solution
+-----------------------
+
+I was SEVERELY mislead because I thought this could be done using
+the sliding window technique. However I was mistaken because of the
+fact that there is no way to easily determine whether or not the endIndex
+is the final endIndex
+
+The idea behind my solution is by using either a hashmap or a vector of length
+2n + 1, you could track the value of the count and look to see if the count 
+has been that value before. Then you just take the difference between the indices
+where the counts were equal since you know that there had to be equal amounts of
+1s and 0s and take the maximum of all of them to find the solution
+
+Complexity
+----------
+Time: O(n)
+Space: O(n) 
+*/
+
+class Solution {
+public:
+  int findMaxLength(vector<int>& nums) {  
+    const int n = nums.size();
+
+    if (n == 1)
+      return 0;
+
+    unordered_map<int,int> idxMap; // (count, index)
+    idxMap.insert({0, -1});
+    int cnt = 0, ans = 0;
+
+    for (int i = 0; i < n; i++) {
+      cnt += nums[i] == 1 ? 1 : -1;
+
+      if (idxMap.count(cnt) == 1) {
+        ans = max(ans, i - idxMap.at(cnt));
+      } else {
+        idxMap.insert({cnt, i});
+      }
+    }
+
+    return ans;
+  }
+};
